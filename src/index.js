fd524f59fef2ef8db64c8fd2b7ec71670474fa92
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-03-04 23:55:29 +0530

    added robots,tachyons for styling n animations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList'
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'tachyons';
+import {robots} from './robots';
+ReactDOM.render(
+<div>
+      <CardList robots={robots}></CardList>
+</div>, 
+document.getElementById('root'));
 registerServiceWorker();
---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+{
+    id : 1,
+    name : 'Prateek Madaan',
+    username : 'prateek951',
+    email : 'prince.cenation@gmail.com'
+},
+{
+    id : 2,
+    name : 'Karunya Mehta',
+    username : 'karunya22',
+    email : 'rose_karu@yahoo.com'
+},
+{
+    id : 3,
+    name : 'Shlok',
+    username : 'shlok2',
+    email : 'shlok77@gmail.com'
+},
+{
+    id : 4,
+    name : 'Nischal Jakhar',
+    username : 'nischal22',
+    email : 'nischaljakhar@gmail.com'
+},
+{
+    id : 5,
+    name : 'Kapil Verma',
+    username : 'kpv22',
+    email : 'kpliplverma@gmail.com'
+},
+{
+    id : 6,
+    name : 'Karunya Mehta',
+    username : 'karunya22',
+    email : 'rose_karu@yahoo.com'
+},
+{
+    id : 7,
+    name : 'Shlok',
+    username : 'shlok2',
+    email : 'shlok77@gmail.com'
+},
+{
+    id : 8,
+    name : 'Nischal Jakhar',
+    username : 'nischal22',
+    email : 'nischaljakhar@gmail.com'
+},
+{
+    id : 9,
+    name : 'Kapil Verma',
+    username : 'kpv22',
+    email : 'kpliplverma@gmail.com'
+}
+];
